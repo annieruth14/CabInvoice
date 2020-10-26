adcbf9cbd61ac82dc7a6088b02281233e80aa240
annieruth14 <ruthannie3@gmail.com>
2020-10-26 20:51:18 +0530

    [Annie]Added: test cases checking total fare for Premium rides as well

--- src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -15,15 +15,15 @@ public class InvoiceServiceTest {
 	public void loadData() {
 		invoiceService = new InvoiceService();
 		rideList = new ArrayList<>();
-		rideList.add(new Ride(2.0, 5));
-		rideList.add(new Ride(0.1, 1));
+		rideList.add(new Ride("Normal", 2.0, 5));
+		rideList.add(new Ride("Premium", 0.1, 1));
 	}
 
 	@Test
 	public void givenDistanceAndTime_shouldReturnTotalFare() {
 		double distance = 2.0;
 		int time = 5;
-		double fare = invoiceService.calculateFare(distance, time);
+		double fare = invoiceService.calculateFareforNormalRide(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
 
@@ -31,14 +31,30 @@ public class InvoiceServiceTest {
 	public void givenLessDistanceAndTime_shouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 1;
-		double fare = invoiceService.calculateFare(distance, time);
+		double fare = invoiceService.calculateFareforNormalRide(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
+	}
+	
+	@Test
+	public void givenDistanceAndTimeForPremiumRides_shouldReturnTotalFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceService.calculateFareforPremiumRide(distance, time);
+		Assert.assertEquals(40, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTimeForPremiumRides_shouldReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceService.calculateFareforPremiumRide(distance, time);
+		Assert.assertEquals(20, fare, 0.0);
 	}
 
 	@Test
 	public void givenMultipleRides_shouldReturnInvoiceSummary() {
 		InvoiceSummary summary = invoiceService.calculateFare(rideList);
-		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30, 15);
+		InvoiceSummary expectedSummary = new InvoiceSummary(2, 45.0, 22.5);
 		Assert.assertEquals(expectedSummary, summary);
 	}
 
@@ -47,7 +63,7 @@ public class InvoiceServiceTest {
 		String userId = "a@b.com";
 		invoiceService.addRides(userId, rideList);
 		InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
-		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30, 15);
+		InvoiceSummary expectedSummary = new InvoiceSummary(2, 45.0, 22.5);
 		Assert.assertEquals(expectedSummary, summary);
 	}
 }
